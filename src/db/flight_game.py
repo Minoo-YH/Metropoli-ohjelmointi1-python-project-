cbdd5ee014f99f2805b1980da25165286e543591
Abdullah22-22 <abdullahaltameemi28@gmail.com>
2025-10-10 14:40:23 +0300

    Save my changes before switching branch

--- src/db/flight_game.py
+++ src/db/flight_game.py
@@ -1,12 +1,21 @@
-﻿import mariadb
+﻿import mysql.connector,os
+from dotenv import load_dotenv
+
+
+load_dotenv()
 
 def get_connection():
-    return mariadb.connect(
-        host="127.0.0.1",
-        user="root",          
-        password="12345",     
-        database="flight_game",
-        port=3307
+    return mysql.connector.connect(
+        host=os.getenv("DB_HOST"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        database=os.getenv("DB_NAME")
     )
+
+
+
+
+
+
 
 
